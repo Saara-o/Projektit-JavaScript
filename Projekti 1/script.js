9a325f53a2accf6834f428c0412ecf7816c6fc8c
Saara <saara.orvas@student.laurea.fi>
2025-10-14 17:19:18 +0300

    Lisätty sovellusvaatimukset sctipt.js tiedostoon
    Co-authored-by: AtteLiikanen <KuningasVoittamaton1@users.noreply.github.com>

---
+++ Projekti 1/script.js
@@ -0,0 +1,20 @@
+// Syöttökentän sisällön tarkistaminen (tyhjä tai liian lyhyt)
+
+// Virheellinen sisältö muuttaa kentän korostuksen punaiseksi
+
+// Listasta voi poistaa elementin
+
+// Listasta voi merkitä tehtävän tehdyksi
+
+// Lisätoiminto: alasvetovalikko/valintalaatikko
+
+// Lisätoiminto: laskuri
+
+// Lisätoiminto: tapa piilottaa elementtejä sovelluksessa
+
+// Lisätoiminto: HTML5:n drag&drop -toiminto
+
+// Lisätoiminto: Nappi -> aktiivinen
+
+// Lisätoiminto Nappi -> tehdyt
+
