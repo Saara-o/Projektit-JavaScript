60116b11dfdaa3b36cd8cef12eb499d2515c8ed4
Saara <saara.orvas@student.laurea.fi>
2025-10-22 13:53:45 +0300

    Lisätty toiminnallisuudet

--- Projekti 1/script.js
+++ Projekti 1/script.js
@@ -18,19 +18,72 @@
 
 // Lisätoiminto Nappi -> tehdyt
 
-
-const uusitodo = document.getElementById('uusi-todo');
-const todolista = document.getElementById('todo-lista');
-const todoMr = document.getElementById('todo-maara');
-const filtteriBtn = document.querySelectorAll('.filtterien napit');
+const uusitodo = document.getElementById("uusi-todo");
+const todolista = document.getElementById("todo-lista");
+const todoMr = document.getElementById("todo-maara");
+const filtteriBtn = document.querySelectorAll(".filtterit button");
 
 let todos = [];
-let filter = 'all';
+let filter = "all";
 
-uusitodo.addEventListener('keypress', (e) => {
-  if (e.key === 'Enter' && e.target.value.trim() !== '') {
+// lisätään tehtävä todo-listatte Enter-näppäimellä
+uusitodo.addEventListener("keypress", (e) => {
+  if (e.key === "Enter" && e.target.value.trim() !== "") {
     todos.push({ text: e.target.value.trim(), completed: false });
-    e.target.value = '';
+    e.target.value = "";
     render();
   }
 });
+
+// tehtävä muutetaan tehdyksi
+function toggleComplete(index) {
+  todos[index].tehty = !todos[index].tehty;
+  render();
+}
+
+// poistetaan todo-listalta tehtävä
+function deleteTodo(index) {
+  todos.splice(index, 1);
+  render();
+}
+
+// todo-lista
+function render() {
+  todolista.innerHTML = "";
+
+  // filtteröidään todo-listan tehtävät
+  const filtteröity = todos.filter(todo =>
+    filter === "aktiivi" ? !todo.tehty :
+    filter === "tehty" ? todo.tehty : true
+  );
+
+  // lisää listaan näkyviin tehtäviä
+  filtteröity.forEach((todo, i) => {
+    const li = document.createElement('li');
+    if (todo.tehty) li.classList.add('tehty');
+
+    li.innerHTML = `
+      <input type="checkbox" ${todo.tehty ? "checked" : ''} onclick="toggleComplete(${i})">
+      <span>${todo.text}</span>
+      <button onclick="deleteTodo(${i})">×</button>
+    `;
+    todolista.appendChild(li);
+  });
+
+  // lasketaan tekemättömien tehtävien määrä
+  const aktiiviMr = todos.filter(t => !t.tehty).length;
+  todoMr.textContent = `${aktiiviMr} item${aktiiviMr !== 1 ? "s" : ""} left`;
+}
+
+// muuttaa näppäimet All, Active, Completed musta/punainen sen mukaan, mikä niistä on aktiivinen
+filtteriBtn.forEach(btn => {
+  btn.addEventListener("click", () => {
+    filtteriBtn.forEach(b => b.classList.remove("selected"));
+    btn.classList.add("selected");
+    filter = btn.id.replace("filtteri-", ""); 
+    render();
+  });
+});
+
+
+render();
